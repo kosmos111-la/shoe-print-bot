f9fda910a6ee49e2d762e54a267caf0957dea466
kosmos111-la <kosmos111@mail.ru>
2025-11-08 16:50:00 +0300

    Update index.js

--- modules/stats/index.js
+++ modules/stats/index.js
@@ -1,9 +1,17 @@
+const StatisticsManager = require('./statistics-manager');
+
+let statsModule = null;
+
 module.exports = {
     async initialize() {
+        statsModule = new StatisticsManager();
+        await statsModule.loadStats();
+       
         console.log('✅ Модуль статистики инициализирован');
-        return {
-            updateUserStats: () => {},
-            getGlobalStats: () => ({ totalUsers: 0, totalPhotos: 0 })
-        };
+        return statsModule;
+    },
+   
+    getModule() {
+        return statsModule;
     }
 };
