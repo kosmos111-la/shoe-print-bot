249e189f0088923383e9e1c7ba235b02f3a689ed
kosmos111-la <kosmos111@mail.ru>
2025-12-06 14:03:07 +0300

    Update index.js

--- modules/footprint/index.js
+++ modules/footprint/index.js
@@ -1,10 +1,11 @@
-// modules/footprint/index.js
+// modules/footprint/index.js - ОБНОВЛЁННЫЙ
 const DigitalFootprint = require('./digital-footprint');
 const FootprintDatabase = require('./footprint-database');
 const FootprintManager = require('./footprint-manager');
 const ModelVisualizer = require('./model-visualizer');
 const EnhancedModelVisualizer = require('./enhanced-model-visualizer');
 const TopologyUtils = require('./topology-utils');
+const PointCloudAligner = require('./point-cloud-aligner'); // НОВЫЙ ИМПОРТ
 
 module.exports = {
     DigitalFootprint,
@@ -12,5 +13,6 @@ module.exports = {
     FootprintManager,
     ModelVisualizer,
     EnhancedModelVisualizer,
-    TopologyUtils
+    TopologyUtils,
+    PointCloudAligner // НОВЫЙ ЭКСПОРТ
 };
