78fb3ffded4ee44c7dd5e98eb730e29a34e78d49
kosmos111-la <kosmos111@mail.ru>
2026-02-09 09:12:07 +0300

    Update index.js

--- modules/footprint/index.js
+++ modules/footprint/index.js
@@ -1,18 +1,13 @@
 // modules/footprint/index.js
 
 const SimpleFootprintManager = require('./simple-manager');
-const GeometricAccumulator = require('./accumulator'); // 🔥 ДОБАВЛЯЕМ НОВЫЙ АККУМУЛЯТОР
-const PointTracker = require('./point-tracker');
 const CoordinateSystemConverter = require('./alignment/coordinate-system-converter');
 const CoordinateValidator = require('./alignment/coordinate-validator');
 const TransformationDebugger = require('./alignment/transformation-debugger');
 const ImprovedAligner = require('./alignment/improved-aligner');
 
-// 🔥 ЭКСПОРТИРУЕМ ВСЕ МОДУЛИ
 module.exports = {
     SimpleFootprintManager,
-    GeometricAccumulator,     // 🔥 НОВЫЙ АККУМУЛЯТОР
-    PointTracker,
     CoordinateSystemConverter,
     CoordinateValidator,
     TransformationDebugger,
