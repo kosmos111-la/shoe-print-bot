07219ceca67ffa197d752305afc07574bba90fa2
kosmos111-la <kosmos111@mail.ru>
2026-03-12 08:19:18 +0300

    Update TriangleMatcher.js

--- modules/footprint/matching/TriangleMatcher.js
+++ modules/footprint/matching/TriangleMatcher.js
@@ -8,7 +8,7 @@ class TriangleMatcher {
 
         // 🔥 НАСТРОЙКИ ЭТАПОВ
         this.roughThreshold = options.roughThreshold || 0.5;      // Порог для грубого поиска
-        this.exactThreshold = options.exactThreshold || 0.9;     // Порог для точной проверки
+        this.exactThreshold = options.exactThreshold || 0.88;     // Порог для точной проверки
 
         // Грубые признаки (этап 1)
         this.roughFeatures = {
