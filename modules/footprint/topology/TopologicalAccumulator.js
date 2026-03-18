5545064cf18f9afa4a898d3f8f72673e40e3076d
kosmos111-la <kosmos111@mail.ru>
2026-03-18 15:11:14 +0300

    Update TopologicalAccumulator.js

--- modules/footprint/topology/TopologicalAccumulator.js
+++ modules/footprint/topology/TopologicalAccumulator.js
@@ -481,7 +481,7 @@ class TopologicalAccumulator {
                             exactGraph,
                             existingModel.graph,
                             finalTransform,
-                            20 // порог в пикселях
+                            25 // порог в пикселях
                         );
 
                         if (pulledCount > 0) {
