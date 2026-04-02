b433df486d0118c80008f5fc91c21cd354b3cf5b
kosmos111-la <kosmos111@mail.ru>
2026-04-02 12:05:09 +0300

    Update TopologicalAccumulator.js

--- modules/footprint/topology/TopologicalAccumulator.js
+++ modules/footprint/topology/TopologicalAccumulator.js
@@ -18,7 +18,7 @@ const AffineRefiner = require('./AffineRefiner');
 class TopologicalAccumulator {
     constructor(options = {}) {
         this.name = options.name || `Топологическая_модель_${Date.now()}`;
-        this.debug = options.debug || true;
+        this.debug = options.debug || false;
 
         // 🔥 РЕЖИМЫ РАБОТЫ
         this.fastMode = options.fastMode || false;
